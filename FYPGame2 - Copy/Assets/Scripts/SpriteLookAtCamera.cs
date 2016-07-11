fcaa7edd01f286fae07581c6302983aafa05696f
dexterthefourth <lawzhixuan@hotmail.com>
2016-07-11 16:35:57 +0800

    Build 1.3.1
    Rewritten code for player tags in the game and removed some unnecessary
    files

--- FYPGame2 - Copy/Assets/Scripts/SpriteLookAtCamera.cs
+++ FYPGame2 - Copy/Assets/Scripts/SpriteLookAtCamera.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class SpriteLookAtCamera : MonoBehaviour {
-    public Camera mainCamera;
+    //public Camera mainCamera;
+    public Transform cameraTransform;
 
     // Use this for initialization
     void Start () {
@@ -11,7 +12,10 @@ public class SpriteLookAtCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         // Look at the opposite of the camera
         transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+        */
+        transform.LookAt(cameraTransform);
     }
 }
